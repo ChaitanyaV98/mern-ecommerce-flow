46071433b1ce5a25db2057388d9429efb8bc6d1c
ChaitanyaV98 <chaitanya@betalectic.com>
2025-05-07 13:56:08 +0530

    added admin products backend crud

--- server/controllers/admin/products-controller.js
+++ server/controllers/admin/products-controller.js
@@ -1,4 +1,5 @@
 import { imageUploadUtil } from "../../helpers/cloudinary.js";
+import Product from "../../models/Product.js";
 
 export const handleImageUpload = async (req, res) => {
   try {
@@ -16,6 +17,135 @@ export const handleImageUpload = async (req, res) => {
     return res.status(500).json({
       success: false,
       message: "Error occured! Please try again ",
+    });
+  }
+};
+
+//add product
+
+export const addProduct = async (req, res) => {
+  try {
+    const {
+      image,
+      title,
+      description,
+      category,
+      price,
+      salePrice,
+      totalStock,
+    } = req.body;
+
+    const newlyCreatedProduct = new Product({
+      image,
+      title,
+      description,
+      category,
+      price,
+      salePrice,
+      totalStock,
+    });
+    await newlyCreatedProduct.save();
+
+    return res.status(201).json({
+      success: true,
+      message: "New product is added successfully",
+      data: newlyCreatedProduct,
+    });
+  } catch (error) {
+    console.log("Error while adding a product", error);
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong! Please try again",
+    });
+  }
+};
+
+//fetch all products
+
+export const fetchAllProducts = async (req, res) => {
+  try {
+    const listOfProducts = await Product.find({});
+
+    if (listOfProducts) {
+      return res.status(200).json({
+        success: true,
+        data: listOfProducts,
+      });
+    }
+  } catch (error) {
+    console.log("Error while fetching products", error);
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong! Please try again",
+    });
+  }
+};
+
+//edit a product
+
+export const updateProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      image,
+      title,
+      description,
+      category,
+      price,
+      salePrice,
+      totalStock,
+    } = req.body;
+    const findProduct = await Product.find(id);
+    if (!findProduct) {
+      res.status(404).json({
+        success: false,
+        message: "Product not found ",
+      });
+    }
+    findProduct.title = title || findProduct.title;
+    findProduct.description = description || findProduct.description;
+    findProduct.category = category || findProduct.category;
+    findProduct.brand = brand || findProduct.brand;
+    findProduct.price = price === "" ? 0 : price || findProduct.price;
+    findProduct.salePrice =
+      salePrice === "" ? 0 : salePrice || findProduct.salePrice;
+    findProduct.totalStock = totalStock || findProduct.totalStock;
+    findProduct.image = image || findProduct.image;
+
+    await findProduct.save();
+    return res.status(200).json({
+      success: true,
+      data: findProduct,
+    });
+  } catch (error) {
+    console.log("Error while updating product", error);
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong! Please try again",
+    });
+  }
+};
+
+//delete a product
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product)
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    return res.status(200).json({
+      success: true,
+      message: "Product delete successfully",
+    });
+  } catch (error) {
+    console.log("Error while deleting  product", error);
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong! Please try again",
     });
   }
 };
--- server/routes/admin/product-routes.js
+++ server/routes/admin/product-routes.js
@@ -1,6 +1,12 @@
 import express from "express";
 import { handleImageUpload } from "../../controllers/admin/products-controller.js";
 import { upload } from "../../helpers/cloudinary.js";
+import {
+  addProduct,
+  updateProduct,
+  fetchAllProducts,
+  deleteProduct,
+} from "../../controllers/admin/products-controller.js";
 
 const adminProductRouter = express.Router();
 
@@ -9,5 +15,10 @@ adminProductRouter.post(
   upload.single("my-file"),
   handleImageUpload
 );
+
+adminProductRouter.post("/add", addProduct);
+adminProductRouter.put("/edit/:id", updateProduct);
+adminProductRouter.delete("/delete/:id", deleteProduct);
+adminProductRouter.get("/get", fetchAllProducts);
 
 export default adminProductRouter;
