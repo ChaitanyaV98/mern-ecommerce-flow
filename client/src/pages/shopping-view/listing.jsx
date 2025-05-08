e945ce79cc2d5b4f63a175f2b67fb09e11d016be
ChaitanyaV98 <chaitanya@betalectic.com>
2025-05-08 17:40:20 +0530

    added filter ui and listing ui for shop view

--- client/src/pages/shopping-view/listing.jsx
+++ client/src/pages/shopping-view/listing.jsx
@@ -1,7 +1,71 @@
+import ProductFilter from "@/components/shopping-view/filter";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ArrowUpDownIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { sortOptions } from "@/config";
+import { fetchAllFilteredProducts } from "@/store/shop/productSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import ShoppingProductTile from "@/components/shopping-view/product-tile";
+
 function ShoppingListing() {
+  const dispatch = useDispatch();
+
+  const { productList } = useSelector((state) => state.shopProducts);
+
+  console.log("Products List", productList);
+
+  useEffect(() => {
+    dispatch(fetchAllFilteredProducts());
+  }, []);
   return (
-    <div>
-      <h1>Shopping product listing </h1>
+    <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] ">
+      <ProductFilter />
+      <div className="bg-background w-full rounded-lg shadow-sm">
+        <div className="p-4 border-b flex items-center justify-between">
+          <h2 className="text-lg font-bold">All Products</h2>
+
+          <div className="flex items-center gap-3">
+            <span className="text-muted-foreground">
+              {productList.length} Products
+            </span>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className="flex items-center gap-1"
+                >
+                  <ArrowUpDownIcon className="h-4 w-4" />
+                  <span>Sort by</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end" className="w-[200px]">
+                <DropdownMenuRadioGroup>
+                  {sortOptions.map((sortItem) => (
+                    <DropdownMenuRadioItem key={sortItem.id}>
+                      {sortItem.label}
+                    </DropdownMenuRadioItem>
+                  ))}
+                </DropdownMenuRadioGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4  p-4">
+          {productList && productList.length > 0
+            ? productList.map((prodItem, index) => (
+                <ShoppingProductTile key={index} product={prodItem} />
+              ))
+            : null}
+        </div>
+      </div>
     </div>
   );
 }
