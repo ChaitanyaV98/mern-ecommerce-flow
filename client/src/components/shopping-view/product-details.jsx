4c1ed4eec187a2046641693041d3d6c7b7416d1c
ChaitanyaV98 <chaitanya@betalectic.com>
2025-05-11 17:53:28 +0530

    added dummy product details model functionality we will add later

--- client/src/components/shopping-view/product-details.jsx
+++ client/src/components/shopping-view/product-details.jsx
@@ -1,4 +1,9 @@
 import { Dialog, DialogContent } from "../ui/dialog";
+import { Button } from "../ui/button";
+import { Separator } from "../ui/separator";
+import { Avatar, AvatarFallback } from "../ui/avatar";
+import { StarIcon } from "lucide-react";
+import { Input } from "../ui/input";
 
 function ProductDetailsDialog({ open, setOpen, productDetails }) {
   return (
@@ -13,12 +18,119 @@ function ProductDetailsDialog({ open, setOpen, productDetails }) {
             className="aspect-sqaure w-full object-cover"
           />
         </div>
-        <div className="grid  gap-6">
+        <div className="">
           <div>
             <h1 className="text-3xl font-bold ">{productDetails?.title}</h1>
-            <p className="text-muted-foreground ">
+            <p className="text-muted-foreground text-2xl mb-5 mt-4  ">
               {productDetails?.description}
             </p>
+          </div>
+          <div className="flex items-center justify-between">
+            <p
+              className={`text-3xl font-bold text-primary ${
+                productDetails?.salePrice > 0 ? "line-through" : ""
+              }`}
+            >
+              ${productDetails?.price}
+            </p>
+            {productDetails?.salePrice > 0 ? (
+              <p className="text-2xl font-bold text-muted-foreground">
+                ${productDetails?.salePrice}
+              </p>
+            ) : null}
+          </div>
+          <div className="flex items-center gap-2 mt-2">
+            <div className="flex items-center gap-0.5 ">
+              <StarIcon className="w-5 h-5 fill-primary" />
+              <StarIcon className="w-5 h-5 fill-primary" />
+              <StarIcon className="w-5 h-5 fill-primary" />
+              <StarIcon className="w-5 h-5 fill-primary" />
+              <StarIcon className="w-5 h-5 fill-primary" />
+            </div>
+            <span className="text-muted-foreground">(4.5)</span>
+          </div>
+          <div className="mt-5 mb-5">
+            <Button className="w-full outline-none">Add to Cart</Button>
+          </div>
+          <Separator />
+
+          {/*Here comes the review functionality */}
+
+          <div className="max-h-[300px] overflow-auto">
+            <h2 className="text-xl font-bold mb-4">Reviews</h2>
+            <div className="grid gap-6 ">
+              {/*all review goes here*/}
+              <div className="flex gap-4">
+                <Avatar>
+                  <AvatarFallback className="h-10 w-10 border">
+                    VC
+                  </AvatarFallback>
+                </Avatar>
+                <div className="grid gap-1">
+                  <div className="flex items-center gap-2">
+                    <h3 className="font-bold">Chaitanya Vadthya</h3>
+                  </div>
+                  <div className="flex items-center gap-0.5 ">
+                    <StarIcon className="w-5 h-5 fill-primary" />
+                    <StarIcon className="w-5 h-5 fill-primary" />
+                    <StarIcon className="w-5 h-5 fill-primary" />
+                    <StarIcon className="w-5 h-5 fill-primary" />
+                    <StarIcon className="w-5 h-5 fill-primary" />
+                  </div>
+                  <p className="text-muted-foreground ">
+                    Here comes the product review
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-4">
+                <Avatar>
+                  <AvatarFallback className="h-10 w-10 border">
+                    VC
+                  </AvatarFallback>
+                </Avatar>
+                <div className="grid gap-1">
+                  <div className="flex items-center gap-2">
+                    <h3 className="font-bold">Chaitanya Vadthya</h3>
+                  </div>
+                  <div className="flex items-center gap-0.5 ">
+                    <StarIcon className="w-5 h-5 fill-primary" />
+                    <StarIcon className="w-5 h-5 fill-primary" />
+                    <StarIcon className="w-5 h-5 fill-primary" />
+                    <StarIcon className="w-5 h-5 fill-primary" />
+                    <StarIcon className="w-5 h-5 fill-primary" />
+                  </div>
+                  <p className="text-muted-foreground ">
+                    Here comes the product review
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-4">
+                <Avatar>
+                  <AvatarFallback className="h-10 w-10 border">
+                    VC
+                  </AvatarFallback>
+                </Avatar>
+                <div className="grid gap-1">
+                  <div className="flex items-center gap-2">
+                    <h3 className="font-bold">Chaitanya Vadthya</h3>
+                  </div>
+                  <div className="flex items-center gap-0.5 ">
+                    <StarIcon className="w-5 h-5 fill-primary" />
+                    <StarIcon className="w-5 h-5 fill-primary" />
+                    <StarIcon className="w-5 h-5 fill-primary" />
+                    <StarIcon className="w-5 h-5 fill-primary" />
+                    <StarIcon className="w-5 h-5 fill-primary" />
+                  </div>
+                  <p className="text-muted-foreground ">
+                    Here comes the product review
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="mt-6 flex gap-2">
+              <Input placeholder="Write a review" />
+              <Button>Submit</Button>
+            </div>
           </div>
         </div>
       </DialogContent>
