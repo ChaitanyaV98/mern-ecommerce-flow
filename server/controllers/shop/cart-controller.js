80db4999812a79c5c27711052d735879fbe32154
ChaitanyaV98 <chaitanya@betalectic.com>
2025-05-12 17:54:28 +0530

    added cart model and crud

---
+++ server/controllers/shop/cart-controller.js
@@ -0,0 +1,217 @@
+import Cart from "../../models/Cart";
+import Product from "../../models/Product";
+
+export const addToCart = async (req, res) => {
+  try {
+    const { userId, productId, quantity } = req.body;
+
+    if (!userId || !productId || quantity < 0) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid data provided!",
+      });
+    }
+    //check for product based on product id
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Invalid productId provided!",
+      });
+    }
+    //Find or Create User's Cart
+    let cart = await Cart.findOne({ userId });
+    if (!cart) {
+      cart = new Cart({ userId, items: [] });
+    }
+    //Searches through cart.items to check if the product is already in the cart
+    const findCurrentProductIndex = cart.items.findIndex(
+      (item) => item.productId.toString() === productId
+    );
+
+    if (findCurrentProductIndex === -1) {
+      //if the user is adding the product for first time in the cart then push productId and quantity
+      cart.items.push({ productId, quantity });
+    } else {
+      //increase quantity
+      cart.items[findCurrentProductIndex].quantity += quantity;
+    }
+
+    await cart.save();
+    res.status(200).json({
+      success: true,
+      data: cart,
+    });
+  } catch (e) {
+    console.log("Error", e);
+    return res.status(500).json({
+      success: false,
+      message: "Some error occured. Please try again",
+    });
+  }
+};
+
+export const fetchCartItems = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    if (!userId) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid userId or userId is required ",
+      });
+    }
+    let cart = await Cart.findOne({ userId }).populate({
+      path: "item.productId",
+      select: "image title price salePrice ",
+    });
+    if (!cart) {
+      return res.status(404).json({
+        success: false,
+        message: "cart not found for this user",
+      });
+    }
+    //if the admin deletes the cart item when you are trying to add
+    const validItems = cart.items.filter(
+      (productItem) => productItem.productId
+    );
+    if (validItems.length < cart.items.length) {
+      cart.items = validItems;
+      await cart.save();
+    }
+    const populateCartItems = validItems.map((item) => ({
+      productId: item.productId._id,
+      image: item.productId.image,
+      title: item.productId.title,
+      price: item.productId.price,
+      salePrice: item.productId.salePrice,
+      quantity: item.quantity,
+    }));
+    res.status(200).json({
+      success: true,
+      data: {
+        ...cart._doc,
+        items: populateCartItems,
+      },
+    });
+  } catch (e) {
+    console.log("Error", e);
+    return res.status(500).json({
+      success: false,
+      message: "Some error occured. Please try again",
+    });
+  }
+};
+
+export const updateCartItemsQty = async (req, res) => {
+  try {
+    const { userId, productId, quantity } = req.body;
+
+    if (!userId || !productId || quantity < 0) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid data provided!",
+      });
+    }
+    const cart = await Cart.findOne({ userId });
+    if (!cart) {
+      return res.status(404).json({
+        success: false,
+        message: "cart not found for this user",
+      });
+    }
+    const findCurrentProductIndex = cart.items.findIndex(
+      (item) => item.productId.toString() === productId
+    );
+    if (findCurrentProductIndex === -1) {
+      return res.status(404).json({
+        success: false,
+        message: "Cart item is not present!",
+      });
+    }
+    cart.items[findCurrentProductIndex].quantity = quantity;
+    await cart.save();
+    await cart.populate({
+      path: "items.productId",
+      select: "image title price salePrice ",
+    });
+    const populateCartItems = cart.items.map((item) => ({
+      productId: item.productId ? item.productId._id : null,
+      image: item.productId ? item.productId.image : null,
+      title: item.productId ? item.productId.title : "Product not found",
+      price: item.productId ? item.productId.price : null,
+      salePrice: item.productId ? item.productId.salePrice : null,
+      quantity: item.quantity,
+    }));
+    res.status(200).json({
+      success: true,
+      data: {
+        ...cart._doc,
+        items: populateCartItems,
+      },
+    });
+  } catch (e) {
+    console.log("Error", e);
+    return res.status(500).json({
+      success: false,
+      message: "Some error occured. Please try again",
+    });
+  }
+};
+
+export const deleteCartItem = async (req, res) => {
+  try {
+    const { userId, productId } = req.params;
+    if (!userId || !productId) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid data provided!",
+      });
+    }
+
+    const cart = await Cart.findOne({ userId }).populate({
+      path: "items.productId",
+      select: "image title price salePrice",
+    });
+
+    if (!cart) {
+      return res.status(404).json({
+        success: false,
+        message: "Cart not found!",
+      });
+    }
+
+    cart.items = cart.items.filter(
+      (item) => item.productId._id.toString() !== productId
+    );
+
+    await cart.save();
+
+    await cart.populate({
+      path: "items.productId",
+      select: "image title price salePrice",
+    });
+
+    const populateCartItems = cart.items.map((item) => ({
+      productId: item.productId ? item.productId._id : null,
+      image: item.productId ? item.productId.image : null,
+      title: item.productId ? item.productId.title : "Product not found",
+      price: item.productId ? item.productId.price : null,
+      salePrice: item.productId ? item.productId.salePrice : null,
+      quantity: item.quantity,
+    }));
+
+    res.status(200).json({
+      success: true,
+      data: {
+        ...cart._doc,
+        items: populateCartItems,
+      },
+    });
+  } catch (e) {
+    console.log("Error", e);
+    return res.status(500).json({
+      success: false,
+      message: "Some error occured. Please try again",
+    });
+  }
+};
