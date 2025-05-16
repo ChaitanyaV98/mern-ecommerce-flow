5ce5ea0c9b638b3fe6321fef2b12d4cff72ce5b8
ChaitanyaV98 <chaitanya@betalectic.com>
2025-05-16 17:31:35 +0530

    corrected uri for delete method

--- client/src/services/shop/cart/deleteCartItem.js
+++ client/src/services/shop/cart/deleteCartItem.js
@@ -3,8 +3,9 @@ import axios from "axios";
 export const deleteCartItem = async ({ userId, productId }) => {
   try {
     const response = await axios.delete(
-      `http://localhost:3000/api/shop/cart/delete/${userId}/${productId}`
+      `http://localhost:3000/api/shop/cart/${userId}/${productId}`
     );
+    console.log("response data after delete", response.data);
     return response.data;
   } catch (error) {
     console.log("Error while deleteting", error);
