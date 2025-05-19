c2dd59cf4b4e9ae7406dbdf3f328a4305ad2a9ba
ChaitanyaV98 <chaitanya@betalectic.com>
2025-05-19 08:32:38 +0530

    added addaddress and fetchAddress in controller

--- server/controllers/shop/address-controller.js
+++ server/controllers/shop/address-controller.js
@@ -1,5 +1,27 @@
+import Address from "../../models/Address.js";
+
 export const addAddress = async (req, res) => {
   try {
+    const { userId, address, city, pincode, phone, notes } = req.body;
+    if (!userId || !address || !city || !pincode || !phone || !notes) {
+      res.status(400).json({
+        success: false,
+        message: "Invalid data provided",
+      });
+    }
+    const newlyCreatedAddress = new Address({
+      userId,
+      address,
+      city,
+      pincode,
+      notes,
+      phone,
+    });
+    await newlyCreatedAddress.save();
+    return res.status(201).json({
+      success: true,
+      data: newlyCreatedAddress,
+    });
   } catch (e) {
     console.log("Error while adding address", e);
     res.status(500).json({
@@ -11,6 +33,20 @@ export const addAddress = async (req, res) => {
 
 export const fetchAllAddress = async (req, res) => {
   try {
+    const { userId } = req.params;
+    if (!userId) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid or no userId is passed, UserId is required",
+      });
+    }
+    const addressList = await Address.find({ userId });
+    //if there is no address found we will manage in frontend
+
+    return res.status(200).json({
+      success: true,
+      data: addressList,
+    });
   } catch (e) {
     console.log("Error while fetching address", e);
     res.status(500).json({
