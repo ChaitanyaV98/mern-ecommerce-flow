b29b752bdd09a833f92e94e57e40e6ea49ea953b
ChaitanyaV98 <chaitanya@betalectic.com>
2025-05-19 08:45:50 +0530

    added update and delete address controller parts

--- server/controllers/shop/address-controller.js
+++ server/controllers/shop/address-controller.js
@@ -58,6 +58,34 @@ export const fetchAllAddress = async (req, res) => {
 
 export const editAddress = async (req, res) => {
   try {
+    const { userId, addressId } = req.params;
+    const formData = req.body;
+    if (!userId || !addressId) {
+      return res.status(400).json({
+        success: false,
+        message: "userId and addressId is required",
+      });
+    }
+    //find and update the address with form data
+    const address = await Address.findOneAndUpdate(
+      {
+        _id: addressId,
+        userId,
+      },
+      formData,
+      { new: true }
+    );
+    if (!address) {
+      return res.status(404).json({
+        success: false,
+        message: "Address Not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "data updated successfully",
+      data: address,
+    });
   } catch (e) {
     console.log("Error while editing address", e);
     res.status(500).json({
@@ -69,6 +97,27 @@ export const editAddress = async (req, res) => {
 
 export const deleteAddress = async (req, res) => {
   try {
+    const { userId, addressId } = req.params;
+    if (!userId || !addressId) {
+      return res.status(400).json({
+        success: false,
+        message: "userId and addressId is required",
+      });
+    }
+    const address = await Address.findOneAndDelete({
+      _id: addressId,
+      userId,
+    });
+    if (!address) {
+      return res.status(404).json({
+        success: false,
+        message: "Address Not found",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Address Deleted suc cessfully",
+    });
   } catch (e) {
     console.log("Error while delete address", e);
     res.status(500).json({
