7c1233d126e9f84541e34e9aeafea525e6c0178d
ChaitanyaV98 <chaitanya@betalectic.com>
2025-05-28 08:29:22 +0530

    added route for payment success

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,6 +22,7 @@ import NotFound from "./pages/not-found";
 import { useEffect } from "react";
 import { checkAuth } from "./store/auth-slice";
 import PaypalReturnPage from "./pages/shopping-view/paypal-return";
+import PaymentSuccess from "./pages/shopping-view/payment-success";
 
 function App() {
   // const isaAuthenticated = true;
@@ -78,6 +79,7 @@ function App() {
           <Route path="checkout" element={<ShoppingCheckout />} />
           <Route path="account" element={<ShoppingAccount />} />
           <Route path="paypal-return" element={<PaypalReturnPage />} />
+          <Route path="payment-success" element={<PaymentSuccess />} />
         </Route>
         <Route path="/unauth-page" element={<UnauthPage />} />
         <Route path="*" element={<NotFound />} />
