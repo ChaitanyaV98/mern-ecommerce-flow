374e37991a5c3aebd960e61c1b8596c31caac559
ChaitanyaV98 <chaitanya@betalectic.com>
2025-05-30 17:10:10 +0530

    updated orders page with actual data

--- client/src/components/shopping-view/orders.jsx
+++ client/src/components/shopping-view/orders.jsx
@@ -8,12 +8,45 @@ import {
   TableCell,
 } from "../ui/table";
 import { Button } from "../ui/button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ShoppingOrderDetailsView from "@/components/shopping-view/order-details";
 import { Dialog } from "../ui/dialog";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getAllOrdersByUser,
+  getOrderDetails,
+  resetOrderDetails,
+} from "@/store/order-slice";
+import { Badge } from "../ui/badge";
 
 function ShoppingOrders() {
   const [openDetailsDialog, setOpenDetailsDialog] = useState(false);
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.auth);
+  const { orderList, orderDetails } = useSelector((state) => state.shopOrder);
+
+  const handleFetchOrderDetails = async (currId) => {
+    try {
+      await dispatch(getOrderDetails(currId));
+    } catch (e) {
+      console.log("ERROR", e);
+    }
+  };
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      await dispatch(getAllOrdersByUser(user?.id));
+    };
+
+    fetchOrders();
+  }, [dispatch, user?.id]);
+
+  useEffect(() => {
+    if (orderDetails !== null) {
+      setOpenDetailsDialog(true);
+    }
+  }, [orderDetails]);
   return (
     <Card>
       <CardHeader>
@@ -33,25 +66,47 @@ function ShoppingOrders() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            <TableRow>
-              <TableCell>1234567</TableCell>
-              <TableCell>20/05/2025</TableCell>
-              <TableCell>In Process</TableCell>
+            {orderList && orderList.length > 0
+              ? orderList.map((orderItem, index) => (
+                  <TableRow key={index}>
+                    <TableCell>{orderItem?._id}</TableCell>
+                    <TableCell>{orderItem?.orderDate.split("T")[0]}</TableCell>
+                    <TableCell>
+                      <Badge
+                        className={`py-1 px-3 ${
+                          orderItem?.orderStatus === "confirmed"
+                            ? "bg-green-500"
+                            : "bg-red-300"
+                        }`}
+                      >
+                        {orderItem?.orderStatus}
+                      </Badge>
+                    </TableCell>
 
-              <TableCell>$1000</TableCell>
+                    <TableCell>{orderItem?.totalAmount}</TableCell>
 
-              <TableCell>
-                <Dialog
-                  open={openDetailsDialog}
-                  onOpenChange={setOpenDetailsDialog}
-                >
-                  <Button onClick={() => setOpenDetailsDialog(true)}>
-                    View Details
-                  </Button>
-                  <ShoppingOrderDetailsView />
-                </Dialog>
-              </TableCell>
-            </TableRow>
+                    <TableCell>
+                      <Dialog
+                        open={openDetailsDialog}
+                        onOpenChange={() => {
+                          setOpenDetailsDialog(false);
+                          dispatch(resetOrderDetails());
+                        }}
+                      >
+                        <Button
+                          onClick={() => {
+                            console.log("Clicked on view details");
+                            handleFetchOrderDetails(orderItem?._id);
+                          }}
+                        >
+                          View Details
+                        </Button>
+                        <ShoppingOrderDetailsView orderDetails={orderDetails} />
+                      </Dialog>
+                    </TableCell>
+                  </TableRow>
+                ))
+              : null}
           </TableBody>
         </Table>
       </CardContent>
