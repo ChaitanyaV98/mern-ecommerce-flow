778fc375d752813822f5cf0ab918851bd784f19f
ChaitanyaV98 <chaitanya@betalectic.com>
2025-05-30 19:22:22 +0530

    updated admin routes with update order

--- server/routes/admin/order-routes.js
+++ server/routes/admin/order-routes.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   getAllOrdersOfAllUsers,
   getOrderDetailsForAdmin,
+  updateOrderStatus,
 } from "../../controllers/admin/order-controller.js";
 
 const adminOrderRouter = express.Router();
 
 adminOrderRouter.get("/orders", getAllOrdersOfAllUsers);
 adminOrderRouter.get("/details/:id", getOrderDetailsForAdmin);
+adminOrderRouter.put("/update/:id", updateOrderStatus);
 
 export default adminOrderRouter;
