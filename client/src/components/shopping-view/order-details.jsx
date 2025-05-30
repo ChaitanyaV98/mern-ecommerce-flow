05aaadde3eab3b11315b12e42ba92b9a095601fa
ChaitanyaV98 <chaitanya@betalectic.com>
2025-05-30 17:06:27 +0530

    updated order details with actual data

--- client/src/components/shopping-view/order-details.jsx
+++ client/src/components/shopping-view/order-details.jsx
@@ -1,8 +1,11 @@
 import { DialogContent } from "../ui/dialog";
 import { Label } from "../ui/label";
 import { Separator } from "../ui/separator";
+import { Badge } from "../ui/badge";
+import { useSelector } from "react-redux";
 
-function ShoppingOrderDetailsView() {
+function ShoppingOrderDetailsView({ orderDetails }) {
+  const { user } = useSelector((state) => state.auth);
   return (
     <DialogContent className="sm:max-w-[600px]">
       <div className="grid gap-6">
@@ -10,25 +13,46 @@ function ShoppingOrderDetailsView() {
           <div className="mt-6 flex items-center justify-between">
             <p className="font-medium ">
               Order Id
-              <Label>123456 </Label>
+              <Label>{orderDetails?._id} </Label>
             </p>
           </div>
           <div className="mt-2 flex items-center justify-between">
             <p className="font-medium ">
               Order Date
-              <Label>20/5/2025 </Label>
+              <Label>{orderDetails?.orderDate.split("T")[0]}</Label>
             </p>
+          </div>
+          <div className="flex mt-2 items-center justify-between">
+            <p className="font-medium">Payment method</p>
+            <Label>{orderDetails?.paymentMethod}</Label>
+          </div>
+          <div className="flex mt-2 items-center justify-between">
+            <p className="font-medium">Payment Status</p>
+            <Label>{orderDetails?.paymentStatus}</Label>
           </div>
           <div className="mt-2 flex items-center justify-between">
             <p className="font-medium ">
               Order Status
-              <Label>In process </Label>
+              <Label>
+                {" "}
+                <Badge
+                  className={`py-1 px-3 ${
+                    orderDetails?.orderStatus === "confirmed"
+                      ? "bg-green-500"
+                      : orderDetails?.orderStatus === "rejected"
+                      ? "bg-red-600"
+                      : "bg-black"
+                  }`}
+                >
+                  {orderDetails?.orderStatus}
+                </Badge>{" "}
+              </Label>
             </p>
           </div>
           <div className="mt-2 flex items-center justify-between">
             <p className="font-medium ">
               Order Price
-              <Label>$1000 </Label>
+              <Label>${orderDetails?.totalAmount} </Label>
             </p>
           </div>
         </div>
@@ -36,11 +60,16 @@ function ShoppingOrderDetailsView() {
         <div className="grid gap-4">
           <div className="grid gap-2 ">
             <div className="font-medium">Order Details</div>
-            <ul className="grid gap-3 ">
-              <li className="flex items-center justify-between">
-                <span>Product one </span>
-                <span>$100</span>
-              </li>
+            <ul className="grid gap-3">
+              {orderDetails?.cartItems && orderDetails?.cartItems.length > 0
+                ? orderDetails?.cartItems.map((item) => (
+                    <li className="flex items-center justify-between">
+                      <span>Title: {item.title}</span>
+                      <span>Quantity: {item.quantity}</span>
+                      <span>Price: ${item.price}</span>
+                    </li>
+                  ))
+                : null}
             </ul>
           </div>
         </div>
@@ -48,12 +77,12 @@ function ShoppingOrderDetailsView() {
           <div className="grid gap-2">
             <div className="font-medium">Shipping Info</div>
             <div className="grid gap-0.5 text-muted-foreground">
-              <span>Chaitanya</span>
-              <span>Address</span>
-              <span>City</span>
-              <span>Pincode</span>
-              <span>Phone</span>
-              <span>Notes</span>
+              <span>{user?.username}</span>
+              <span>{orderDetails?.addressInfo?.address}</span>
+              <span>{orderDetails?.addressInfo?.city}</span>
+              <span>{orderDetails?.addressInfo?.pincode}</span>
+              <span>{orderDetails?.addressInfo?.phone}</span>
+              <span>{orderDetails?.addressInfo?.notes}</span>
             </div>
           </div>
         </div>
