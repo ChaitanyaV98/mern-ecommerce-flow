030553ad28104688cb2d3f28d72c804561670c5f
ChaitanyaV98 <chaitanya@betalectic.com>
2025-05-30 13:03:47 +0530

    updated routes in order routes

--- server/routes/shop/order-routes.js
+++ server/routes/shop/order-routes.js
@@ -2,11 +2,15 @@ import express from "express";
 import {
   createOrder,
   capturePayment,
+  getAllOrdersByUser,
+  getOrderDetails,
 } from "../../controllers/shop/order-controller.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/create-order", createOrder);
 orderRouter.post("/capture-payment", capturePayment);
+orderRouter.get("/list/:userId", getAllOrdersByUser);
+orderRouter.get("/details/:id", getOrderDetails);
 
 export default orderRouter;
